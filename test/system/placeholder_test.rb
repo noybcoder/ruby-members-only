766555031523609a3046ec3a008b2f6b2f7159ac
Preston Lam <preslam911@gmail.com>
2026-02-17 00:26:21 -0700

    commented out the system test section in .github/workflows/ci.yml

--- test/system/placeholder_test.rb
+++
@@ -1,9 +0,0 @@
-# test/system/placeholder_test.rb
-require "application_system_test_case"
-
-class PlaceholderTest < ApplicationSystemTestCase
-  test "visiting the index" do
-    # This is just a placeholder test
-    assert true
-  end
-end
