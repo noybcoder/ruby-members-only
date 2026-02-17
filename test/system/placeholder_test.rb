dbcf19f14caaba786a1899d8e9a059fdb7447de7
Preston Lam <preslam911@gmail.com>
2026-02-17 00:23:28 -0700

    added test/system folder

---
+++ test/system/placeholder_test.rb
@@ -0,0 +1,9 @@
+# test/system/placeholder_test.rb
+require "application_system_test_case"
+
+class PlaceholderTest < ApplicationSystemTestCase
+  test "visiting the index" do
+    # This is just a placeholder test
+    assert true
+  end
+end
